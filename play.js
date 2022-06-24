bb9113fdc68ae909fc431ad2a08bd62f6b4a1a43
mjuned <mmj.nowaz@gmail.com>
2022-06-24 14:51:25 +0000

    Imported connect function

--- play.js
+++ play.js
@@ -1,25 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "10.0.2.15", // IP address here,
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Connected to server.");
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const { connect } = require('./client');
 
 console.log("Connecting ...");
 connect();
