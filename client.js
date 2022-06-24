8f1e759a10e3a0387ea894ca4e57d78dcf28f4a7
mjuned <mmj.nowaz@gmail.com>
2022-06-24 18:04:31 +0000

    Added two constants from constants.js

--- client.js
+++ client.js
@@ -1,13 +1,12 @@
 const net = require("net");
-
-// establishes a connection with the game server
+const { IP, PORT } = require("./constants");
+/*
 const connect = () => {
   const conn = net.createConnection({
-    host: "10.0.2.15", // IP address here,
+    host: "10.0.2.15",
     port: 50541,
   });
-
-  // interpret incoming data as text
+*/
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
