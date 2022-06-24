681541e740c1a1cdce4b939aa6f53385eaf50436
mjuned <mmj.nowaz@gmail.com>
2022-06-24 18:06:39 +0000

    Added two constants from constants.js

--- client.js
+++ client.js
@@ -1,12 +1,12 @@
 const net = require("net");
 const { IP, PORT } = require("./constants");
-/*
+
 const connect = () => {
   const conn = net.createConnection({
     host: "10.0.2.15",
     port: 50541,
   });
-*/
+
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
