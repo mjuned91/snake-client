db41e08e427e3106bed86be2d0e2611ec7cbc02f
mjuned <mmj.nowaz@gmail.com>
2022-06-24 18:05:40 +0000

    Refactored

--- input.js
+++ input.js
@@ -1,20 +1,23 @@
 let connection;
 
 const handleUserInput = key => {
-  if (key === '\u0003') {
+  if (key === "\u0003") {
     process.exit();
   }
-  if (key === 'w') {
-    connection.write('Move: up');
+  if (key === "w") {
+    connection.write("Move: up");
   }
-  if (key === 'a') {
-    connection.write('Move: left');
+  if (key === "a") {
+    connection.write("Move: left");
   }
-  if (key === 's') {
-    connection.write('Move: down');
+  if (key === "s") {
+    connection.write("Move: down");
   }
-  if (key === 'd') {
-    connection.write('Move: right');
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+  if (key === "f") {
+    connection.write("Say: Finally works!");
   }
 };
 
