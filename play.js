7a60654fdf47a001c533c388c5b42dea3a6b2935
mjuned <mmj.nowaz@gmail.com>
2022-06-24 17:22:52 +0000

    Refactored

--- play.js
+++ play.js
@@ -2,6 +2,7 @@ const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+let connection = connect();
+
+setupInput(connection);
