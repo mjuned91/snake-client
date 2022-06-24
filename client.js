2a85f1b10b5efa5cce46b90dbefd840b28d0649c
mjuned <mmj.nowaz@gmail.com>
2022-06-24 14:50:41 +0000

    Added and exported connect function

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "10.0.2.15", // IP address here,
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Connected to server.");
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
